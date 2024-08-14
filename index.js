74d51914806de6a18dbd7c114fed5d0f5b5e04cb
Simon <simon.vereecke@live.com>
2024-08-15 00:27:36 +0200

    validate email for input

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 window.onload = function () {
-    const submitBtn = document.getElementById('submitBtn');
-    submitBtn.addEventListener("click", (event) => handleClick(event));
+    document.getElementById('submitBtn').addEventListener("click", (event) => handleClick(event));
 };
 
 function handleClick(event) {
-    console.log(event);
     event.preventDefault();
-    
+
+    const inputField = document.getElementById("eMailInput");
+    const isValid = ValidateEmail(inputField.value)
+
+    console.log("is valid: ", isValid);
+    // if(!isValid)
+    //   inputField.classList.add("invalid");
+}
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
 }
