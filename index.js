0ec199bea8108fbd8c45bcd2d9701370872a3c75
Simon <simon.vereecke@live.com>
2024-08-15 00:13:44 +0200

    add eventListener to submit button

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 window.onload = function () {
-    
+    const submitBtn = document.getElementById('submitBtn');
+    submitBtn.addEventListener("click", (event) => handleClick(event));
 };
+
+function handleClick(event) {
+    console.log(event);
+    event.preventDefault();
+    
+}
